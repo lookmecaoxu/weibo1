1368bebfe5c9435ff83bf5817d8477a702db22f5
caoxu <cx_demo@163.com>
2020-01-07 09:50:50 +0000

    csrf令牌

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,9 +11,21 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+    # 个人信息 /user/1
     public function show(User $user)
     {
-
         return view('users.show', compact('user'));
+    }
+
+    # 注册提交 /users post
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
