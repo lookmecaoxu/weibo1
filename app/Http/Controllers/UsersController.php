0492050fd3764450ea2b3047d25199545534bbe4
caoxu <cx_demo@163.com>
2020-01-07 09:22:07 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller
 {
     //注册页面
     public function create()
     {
         return view('users.create');
+    }
+    public function show(User $user)
+    {
+        $user->gravatar();
+        return view('users.show', compact('user'));
     }
 }
